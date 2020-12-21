983dedce96276f0048285b4f61bd43bbfa847dea
Viktor De Pasquale <info@depasquale.wiki>
2020-12-21 09:36:43 +0100

    Updated gradle distribution

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -19,8 +19,8 @@ dependencies {
 
     implementation("com.android.tools.build:gradle:4.1.1")
     implementation("com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.5")
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.20")
-    implementation("com.skoumal.grimoire:cover:1.0.0-alpha04")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.21")
+    implementation("com.skoumal.grimoire:cover:1.0.0-beta01")
 
 }
 
